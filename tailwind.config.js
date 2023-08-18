d1a660f7ac8d596722f46330e56fb3a316177db5
unknown <alijawad1109@gmail.com>
2023-08-18 04:24:19 -0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode:'jit',
+  content: [],
+  theme: {
+    container:{
+      center:true,
+      padding:'1rem',
+      screens:{
+        lg:'1124px',
+        xl:'1124px',
+        '2xl':'1124px',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
